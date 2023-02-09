31ce4bdcd3d1e0fdb8f8fd325e8244a9c98e7510
Nick Santana <nick@mobilecoin.com>
2023-02-09 15:04:11 -0800

    You know the rules and so do I
    working on the cli for this

--- grpcio-gen/src/main.rs
+++ grpcio-gen/src/main.rs
@@ -3,10 +3,23 @@
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use clap::{Parser, Subcommand};
+
+/// Generates rust bindings for grpcio using prost
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Proto files to generate code from
+    #[arg()]
+    protos: Vec<String>,
+
+
+    /// Output directory to place the generated rust files
+    #[arg(short, long)]
+    out_dir: Option<PathBuf>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
 }
