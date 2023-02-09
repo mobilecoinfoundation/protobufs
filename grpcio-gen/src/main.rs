81fe4d1f5009a0d5bdf4a14287fc4c29813054e1
Nick Santana <nick@mobilecoin.com>
2023-02-09 15:29:37 -0800

    adding anyhow since it's pulled in by prost

--- grpcio-gen/src/main.rs
+++ grpcio-gen/src/main.rs
@@ -3,6 +3,8 @@
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 
+use anyhow::Result;
+use std::env::current_dir;
 use std::path::PathBuf;
 use clap::Parser;
 
@@ -11,20 +13,26 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Proto files to generate code from
-    #[clap()]
+    #[clap(value_names = ["PROTO"])]
     protos: Vec<String>,
 
+    /// Include directories to use for dependencies
+    #[clap(short = 'I', long, value_names = ["INCLUDE"])]
+    includes: Vec<String>,
 
     /// Output directory to place the generated rust files
     #[clap(short, long)]
     out_dir: Option<PathBuf>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    println!("Hello {:?}!", cli.out_dir);
-    println!("Hello {:?}!", cli.protos);
-    Ok(())
+    let out_dir = cli.out_dir.unwrap_or_else(|| current_dir().expect("Failed to get current directory."));
 
+    grpcio_compiler::prost_codegen::compile_protos(
+        &cli.protos,
+        &cli.includes,
+        out_dir.as_os_str().to_str().expect("Output directory has invalid UTF-8"),)?;
+    Ok(())
 }
