d96cbbb35c53169f1e6a22622a50eaac50a50a27
Nick Santana <nick@mobilecoin.com>
2023-02-17 09:00:49 -0800

    Simplify code generation for attestation protobufs
    The tonic and prost message build.rs files have been simplified.

--- mobilecoinfoundation/attestation/tonic/build.rs
+++ mobilecoinfoundation/attestation/tonic/build.rs
@@ -2,40 +2,12 @@
 
 //! Generate the tonic code for the gRPC service.
 
-use std::{
-    env,
-    io::{Error, ErrorKind},
-    path::PathBuf,
-    process::Command,
-};
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-    let protoc = prost_build::protoc_from_env();
-    let mut cmd = Command::new(protoc);
-    cmd.arg(format!("--tonic_out={}", out_dir.display()))
-        .arg("--tonic_opt=no_include");
-
-    let protos = &["../v1/services.proto"];
-    let includes = &["../../../", "../"];
-
-    for i in includes {
-        cmd.arg("-I").arg(i);
-    }
-
-    for p in protos {
-        cmd.arg(p);
-    }
-
-    let output = cmd.output()?;
-
-    if !output.status.success() {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        return Err(Error::new(
-            ErrorKind::Other,
-            format!("Failed building tonic gRPC service: {stderr}"),
-        ))?;
-    }
+    let builder = tonic_build::configure().extern_path(
+        ".mobilecoinfoundation.attestation.v1",
+        "::mc_attestation_messages",
+    );
+    builder.compile(&["../v1/services.proto"], &["../../../", "../"])?;
 
     Ok(())
 }
--- mobilecoinfoundation/attestation/tonic/src/lib.rs
+++ mobilecoinfoundation/attestation/tonic/src/lib.rs
@@ -3,9 +3,7 @@
 #![doc = include_str!("../README.md")]
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 
-use mc_attestation_messages::*;
-
 include!(concat!(
     env!("OUT_DIR"),
-    "/mobilecoinfoundation.attestation.v1.tonic.rs"
+    "/mobilecoinfoundation.attestation.v1.rs"
 ));
